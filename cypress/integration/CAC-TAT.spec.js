1eff11e84ff52c3f19f5089bff3ee73dd834d5cc
rqcruz <ronaldocruz183@gmail.com>
2024-10-10 21:34:02 -0300

    test:Adicionando testes da aula 11

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -273,3 +273,83 @@ describe('Aula e exercicios contidos em /lessons/07.md', function() {
    })
 
 })
+
+describe('Aula e exercicios contidos em /lessons/11.md', function() {
+
+    const THREE_SECONDS_IN_MS = 3000
+    
+    beforeEach(function () {
+        cy.visit('./src/index.html')
+    });
+
+    it('Usando funcao clock() e tick() 1', function () {
+        const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste.'
+        
+        cy.clock()
+        
+        cy.get('#firstName').type('João')
+        cy.get('#lastName').type('Grilo')
+        cy.get('#email').type('joao_grilo@tat.com')
+        cy.get('#open-text-area').type(longText, { delay: 0 })
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.success').should('be.visible')
+
+        cy.tick(THREE_SECONDS_IN_MS)
+
+        cy.get('.success').should('not.be.visible')
+    })
+
+    // Exercicio extra 2
+    it('Usando funcao clock() e tick() 2', function () {
+
+        cy.clock()
+        cy.get('#firstName').type('João')
+        cy.get('#lastName').type('Grilo')
+        cy.get('#email').type('email_invalido@')
+        cy.get('#open-text-area').type('Exercicio extra 2 do 02.md')
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+
+        cy.tick(THREE_SECONDS_IN_MS)
+
+        cy.get('.success').should('not.be.visible')
+    })
+
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function() {
+        cy.get('.success')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Mensagem enviada com sucesso.')
+          .invoke('hide')
+          .should('not.be.visible')
+        cy.get('.error')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Valide os campos obrigatórios!')
+          .invoke('hide')
+          .should('not.be.visible')
+    })
+
+    it('preenche a area de texto usando o comando invoke', function () {
+        const longText = Cypress._.repeat('0123456789 ', 20)
+
+        cy.get('#open-text-area')
+            .invoke('val', longText)
+            .should('have.value', longText)
+    })
+
+    it('faz uma requisição HTTP', function() {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function(response) {
+                const {status, statusText, body } = response
+                expect(status).to.equal(200)
+                expect(statusText).to.equal('OK')
+                expect(body).to.include('CAC TAT')
+            })
+    })
+
+})
--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,6 +1,10 @@
-// Exercicios extra 2 contidos em /lessons/07.md
-it('testa a página da política de privacidade de forma independente', function() {
-    cy.visit('./src/privacy.html')
+// /lessons/11.md - Exercício extra 1
+Cypress._.times(3, function() {
 
-    cy.contains('Talking About Testing').should('be.visible')
+    // Exercicios extra 2 contidos em /lessons/07.md
+    it('testa a página da política de privacidade de forma independente', function() {
+        cy.visit('./src/privacy.html')
+
+        cy.contains('Talking About Testing').should('be.visible')
+    })
 })
